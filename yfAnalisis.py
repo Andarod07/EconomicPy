51f662455c3bf260498271e652c893ff56d8df06
Andarod07 <angeldavidbecerra5@gmail.com>
2026-06-01 01:27:38 -0500

    added gitignore

--- yfAnalisis.py
+++ yfAnalisis.py
@@ -4,7 +4,7 @@ import yfinance as yf
 def angelito(symbol):
 	dat = yf.Ticker(symbol)
 	#print(dat.history(period='1y', interval="1wk"))
-	
+
 	dat = dat.history(period='1y', interval="1wk")
 	dat = dat["Close"]
 	print(dat)
