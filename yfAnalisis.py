845df9fa035d13f5879f358311c876e4dc4044be
Andarod07 <angeldavidbecerra5@gmail.com>
2026-06-01 02:23:21 -0500

    added column selection

--- yfAnalisis.py
+++ yfAnalisis.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import yfinance as yf
+from typing import Literal
 
-def fetchData(symbol):
+def fetchData(symbol:str, column:Literal['Open', 'High', 'Low', 'Close', 'Volume','Dividends', 'Stock Splits']=None):
 	dat = yf.Ticker(symbol)
-	#print(dat.history(period='1y', interval="1wk"))
 
 	dat = dat.history(period='1y', interval="1wk")
-	dat = dat["Close"]
-	print(dat)
-	dat.to_excel('Analisis.xlsx', index=False)
+	if column:
+		dat = dat[column]
 
-fetchData("NOMD")
+	print(dat)
+	
+	#dat.to_excel('Analisis.xlsx', index=False)
+
+fetchData("NOMD", column='Dividends')
