5e16269381a093e99ec47d08e801080c18fdfece
Andarod07 <angeldavidbecerra5@gmail.com>
2026-06-01 02:03:54 -0500

    added unfininshed func to calculate period

--- yfAnalisis.py
+++ yfAnalisis.py
@@ -1,13 +1,29 @@
+import re
 import pandas as pd
 import yfinance as yf
 
-def angelito(symbol):
-	dat = yf.Ticker(symbol)
-	#print(dat.history(period='1y', interval="1wk"))
+def calculatePeriod(intervalo=str, cantidad=int):
+	timeInterval = re.sub(r'\d+', '', intervalo)
+	match timeInterval:
+		#intervalo possibilities are: 1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo
+		#period possibilities are: 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max
+		#calculate the period to fetch data based on the interval and cantidad
+		#if the interval is in minutes, the period will be in days
 
-	dat = dat.history(period='1y', interval="1wk")
+		case 'm':
+			calculoPeriodo = '???'
+
+	return calculoPeriodo
+
+def fetchData(symbol=str,intervalo=str, cantidad=int):
+	dat = yf.Ticker(symbol)
+
+		
+	periodo = calculatePeriod(intervalo, cantidad)
+
+	dat = dat.history(period=periodo, interval=intervalo)
 	dat = dat["Close"]
 	print(dat)
-	dat.to_excel('Analisis.xlsx', index=False)
+	#dat.to_excel('Analisis.xlsx', index=False)
 
-angelito("NOMD")
+fetchData("NOMD", "1mo", "1wk")
